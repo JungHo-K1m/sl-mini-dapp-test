9b186eccf1477480fd03e4cbd921feb4f8c7555f
JungHo-K1m <qopqop55@naver.com>
2024-12-08 16:52:51 +0900

    check tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export const darkMode = ['class'];
 export const content = [
@@ -74,4 +76,4 @@ export const theme = {
     },
   },
 };
-export const plugins = [require('tailwindcss-animate')];
+export const plugins = [tailwindcssAnimate];
