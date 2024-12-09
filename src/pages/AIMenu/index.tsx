44a7c6f672fa6f539864336159d68aec43200f12
JungHo-K1m <qopqop55@naver.com>
2024-12-09 13:23:54 +0900

    update translation

--- src/pages/AIMenu/index.tsx
+++ src/pages/AIMenu/index.tsx
@@ -133,7 +133,7 @@ const AIMenu: React.FC = () => {
       {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
             <div className="bg-white text-black p-6 rounded-lg text-center">
-                <p>{t("ai_page.10SL_tokens")}</p>
+                <p>{t("ai_page.5SL_tokens")}</p>
                 <button
                     className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
                     onClick={handleCloseModal}
