8d60636d4a2885fd601fd15099447917c1e01740
JungHo-K1m <qopqop55@naver.com>
2024-12-16 17:41:53 +0900

    test personalInfo Policy

--- src/pages/PolicyDetail/index.tsx
+++ src/pages/PolicyDetail/index.tsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from 'react-router-dom';
 import { TopTitle } from '@/shared/components/ui';
 
 const PolicyDetailPage: React.FC = () => {
+    const [content, setContent] = useState<string>("");
+  
+    // HTML 파일 불러오기
+    useEffect(() => {
+      const fetchContent = async () => {
+        try {
+          const response = await fetch("/policies/personalInfo.html");
+          const htmlContent = await response.text();
+          setContent(htmlContent);
+        } catch (error) {
+          console.error("Failed to load policy content:", error);
+        }
+      };
+  
+      fetchContent();
+    }, []);
 
 
     return(
         <div className="flex flex-col items-center text-white mx-6 min-h-screen">
             <TopTitle title="Policy" back={true} />
-
+            
+            {/* 정책 내용 */}
+            <div
+                className="bg-white text-black p-4 rounded-md shadow-md mt-4 w-full max-w-2xl overflow-y-auto"
+                dangerouslySetInnerHTML={{ __html: content }}
+            />
         </div>
     );
 };
