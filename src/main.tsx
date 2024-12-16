9668af5a175288d115e5bd5af8196dc8c4b6bf78
JungHo-K1m <qopqop55@naver.com>
2024-12-16 11:04:36 +0900

    update App file

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import App from "./App";
-import App from "./app/index"
+import App from './app';
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
