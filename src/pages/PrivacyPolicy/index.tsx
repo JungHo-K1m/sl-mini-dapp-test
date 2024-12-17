26c42b439bfc9ef81b5675f2190bb0b06bc057fb
JungHo-K1m <qopqop55@naver.com>
2024-12-17 20:08:09 +0900

    test Terms of Service page

--- src/pages/PrivacyPolicy/index.tsx
+++ src/pages/PrivacyPolicy/index.tsx
@@ -7,6 +7,12 @@ const PrivacyPolicy: React.FC = () => {
         <div className="flex flex-col items-center bg-transparent text-white mx-6 min-h-screen">
             <TopTitle title="Policy" back={true} />
 
+            
+            {/* 본문 내용 */}
+            <div className="text-left w-full mt-4">
+                <p className="text-xl font-semibold">Privacy Policy</p><br />
+                
+            </div>
         </div>
     );
 };
