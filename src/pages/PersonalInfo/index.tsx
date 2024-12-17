0c45984bc632531555a4fbf6d2256aa36ac42b1e
JungHo-K1m <qopqop55@naver.com>
2024-12-17 18:04:17 +0900

    update Collecting Personal Info Page

--- src/pages/PersonalInfo/index.tsx
+++ src/pages/PersonalInfo/index.tsx
@@ -7,34 +7,36 @@ const CollectingPersonalInfo: React.FC = () => {
         <div className="flex flex-col items-center bg-transparent text-white mx-6 min-h-screen">
             <TopTitle title="Policy" back={true} />
 
-            <h1>Consent to Collection and Use of Personal Information</h1>
-            <p className="text-sm"><br />
-                <strong>Purpose of Collection and Use of Personal Information:</strong><br />
-                Your email information is collected to deliver Newsletters. The collected
-                personal information will not be used for any purpose other than the
-                specified purpose, and in the event of any change in the purpose of the
-                collection, you will be notified in advance for your consent.
-            </p><br />
+            <div className="text-left w-full mt-4">
+                <p className="text-xl">Consent to Collection and Use of Personal Information</p>
+                <p className="text-base"><br />
+                    <strong>Purpose of Collection and Use of Personal Information:</strong><br />
+                    Your email information is collected to deliver Newsletters. The collected
+                    personal information will not be used for any purpose other than the
+                    specified purpose, and in the event of any change in the purpose of the
+                    collection, you will be notified in advance for your consent.
+                </p><br />
 
-            <p className="text-sm">
-                <strong>Personal Information Collected:</strong> Email
-            </p><br />
+                <p className="text-base">
+                    <strong>Personal Information Collected:</strong> Email
+                </p><br />
 
-            <p className="text-sm">
-                <strong>Period of Use and Retention:</strong> 2 years
-            </p><br />
+                <p className="text-base">
+                    <strong>Period of Use and Retention:</strong> 2 years
+                </p><br />
 
-            <p className="text-sm">
-                <strong>Right to Refuse Consent:</strong><br />
-                You have the right to refuse to consent to the collection and use of your
-                personal data, and if you do not agree, you will not be able to receive the
-                newsletter.
-            </p><br />
+                <p className="text-base">
+                    <strong>Right to Refuse Consent:</strong><br />
+                    You have the right to refuse to consent to the collection and use of your
+                    personal data, and if you do not agree, you will not be able to receive the
+                    newsletter.
+                </p><br />
 
-            <p className="text-[#555] text-xs">
-                This consent is required for newsletter subscription. Please read the
-                information carefully before proceeding.
-            </p>
+                <p className="text-[#555] text-sm">
+                    This consent is required for newsletter subscription. Please read the
+                    information carefully before proceeding.
+                </p>
+            </div>
         </div>
     );
 };
