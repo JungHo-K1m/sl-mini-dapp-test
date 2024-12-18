e89a114a751d6ba8a7a2c66f20cadf1f92388d99
JungHo-K1m <qopqop55@naver.com>
2024-12-18 15:22:44 +0900

    change misstake

--- src/pages/PolicyDetail/index.tsx
+++ src/pages/PolicyDetail/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from 'react-router-dom';
-import { TopTitle } from '@/shared/components/ui';
+import { TopTitle } from "@/shared/components/ui";
 
 const PolicyDetailPage: React.FC = () => {
   const [iframeHeight, setIframeHeight] = useState<string>("0px");
@@ -11,8 +10,7 @@ const PolicyDetailPage: React.FC = () => {
       if (event.data?.type === "resizeIframe" && event.data.height) {
         const newHeight = `${event.data.height + 16}px`; // 동적 높이 + 여백
         if (newHeight !== iframeHeight) {
-          // 높이가 변경된 경우에만 업데이트
-          setIframeHeight(newHeight);
+          setIframeHeight(newHeight); // 높이가 변경된 경우에만 업데이트
         }
       }
     };
@@ -22,25 +20,25 @@ const PolicyDetailPage: React.FC = () => {
     return () => {
       window.removeEventListener("message", handleResizeMessage);
     };
-  }, [iframeHeight]); // iframeHeight를 의존성으로 추가
+  }, [iframeHeight]);
 
-  return(
-      <div className="flex flex-col items-center bg-transparent text-white mx-6 min-h-screen">
-          <TopTitle title="Policy" back={true} />
+  return (
+    <div className="flex flex-col items-center bg-transparent text-white mx-6 min-h-screen">
+      <TopTitle title="Policy" back={true} />
 
-          {/* 정책 내용 */}
-          <div className="w-full">
-            <iframe
-              src={iframeSrc}
-              title="Policy Detail"
-              style={{
-                border: "none",
-                width: "100%",
-                height: iframeHeight, // 동적으로 높이 설정
-              }}
-            />
-          </div>
+      {/* 정책 내용 */}
+      <div className="w-full">
+        <iframe
+          src={iframeSrc}
+          title="Policy Detail"
+          style={{
+            border: "none",
+            width: "100%",
+            height: iframeHeight,
+          }}
+        />
       </div>
+    </div>
   );
 };
 
