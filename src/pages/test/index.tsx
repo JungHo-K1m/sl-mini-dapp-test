f402cad6a9c778d18fdd494227380122298cc1a4
JungHo-K1m <qopqop55@naver.com>
2024-12-24 16:14:04 +0900

    add debuging log

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -6,26 +6,28 @@ const WalletConnect: React.FC = () => {
 
   const connectWallet = async () => {
     try {
-      // SDK 초기화
+      console.log("초기화 시작");
       const sdk = await DappPortalSDK.init({
-        clientId: process.env.VITE_LINE_CLIENT_ID || "", // 환경 변수에서 clientId 읽기
+        clientId: process.env.VITE_LINE_CLIENT_ID || "",
       });
-
-      // WalletProvider 가져오기
+  
+      console.log("SDK 초기화 완료:", sdk);
+  
       const walletProvider = sdk.getWalletProvider();
-      console.log("walletProvider 가져오기: ",walletProvider);
-
-      // 지갑 연결 요청
+      console.log("WalletProvider 가져오기 성공:", walletProvider);
+  
       const accounts = (await walletProvider.request({
-        method: "kaia_requestAccounts", // Wallet 연결 요청
-      })) as string[]; // 반환 값을 string 배열로 단언
-
-      setAccount(accounts[0]); // 첫 번째 계정 저장
+        method: "kaia_requestAccounts",
+      })) as string[];
+  
+      setAccount(accounts[0]);
       console.log("지갑 연결 성공:", accounts[0]);
-    } catch (error) {
-      console.error("지갑 연결 실패 또는 clientId 오류:", error);
+    } catch (error: any) {
+      console.error("에러 발생:", error.message);
+      console.error("에러 응답:", error.response?.data || "응답 없음");
     }
   };
+  
 
   return (
     <div className="flex flex-col text-white mb-32 px-6 min-h-screen">
