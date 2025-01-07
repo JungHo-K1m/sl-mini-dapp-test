a04f11103a2c00ee80d04dc2e6db564b2dd12d1d
JungHo-K1m <qopqop55@naver.com>
2025-01-07 10:43:39 +0900

    update splash page

--- src/app/components/SplashScreen.tsx
+++ src/app/components/SplashScreen.tsx
@@ -1,13 +1,19 @@
 // src/components/SplashScreen.tsx
 import React from 'react';
+import Images from '@/shared/assets/images';
 
 const SplashScreen: React.FC = () => {
   return (
-    <div className="w-full h-screen bg-[#0D1226]">
+    <div
+      className="relative w-full h-screen flex flex-col justify-center items-center bg-cover bg-center"
+      style={{
+          backgroundImage: `url(${Images.SplashBackground})`,
+      }}
+    >
       <img
-        src="/splash.png"
-        alt="Splash Screen"
-        className="w-full h-full object-cover"
+        src={Images.SplashTitle}
+        alt="Lucky Dice Logo"
+        className="w-[272px] mb-[90px]"
       />
     </div>
   );
