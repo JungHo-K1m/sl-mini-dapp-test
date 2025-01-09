3574e5b899845197851fc84ace694c27078c220c
JungHo-K1m <qopqop55@naver.com>
2025-01-09 14:51:21 +0900

    check mission page

--- src/pages/MissionPage/index.tsx
+++ src/pages/MissionPage/index.tsx
@@ -84,7 +84,7 @@ const OneTimeMissionCard: React.FC<OneTimeMissionCardProps> = ({
 
       {/* Completed Badge */}
       {mission.isCleared && (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-semibold rounded-full px-4 py-2 z-20 flex items-center justify-center gap-2">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-sm font-semibold rounded-full px-4 py-2 z-20 flex items-center justify-center gap-2">
           <img
             src={Images.MissionCompleted}
             alt="Mission Completed"
@@ -293,7 +293,7 @@ const MissionPage: React.FC = () => {
                 </div>
 
                 {mission.isCleared && (
-                  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-semibold rounded-full px-4 py-2 z-20 flex items-center justify-center gap-2">
+                  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-sm font-semibold rounded-full px-4 py-2 z-20 flex items-center justify-center gap-2">
                     <img
                       src={Images.MissionCompleted}
                       alt="Mission Completed"
