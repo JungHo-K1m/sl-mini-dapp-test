bd547702f35de67377c881c1767f9d8384457704
JungHo-K1m <qopqop55@naver.com>
2025-02-05 17:05:44 +0900

    update UI

--- src/pages/PaymentDetail/index.tsx
+++ src/pages/PaymentDetail/index.tsx
@@ -29,7 +29,7 @@ const PaymentDetail: React.FC = () => {
                     ? "linear-gradient(180deg, #0147E5 0%, #FFFFFF 100%)"
                     : "linear-gradient(180deg, #FF4F4F 0%, #FFFFFF 100%)",
                 }}
-            >
+                >
                 <img
                     src={
                         itemName === "Auto Item"
@@ -40,24 +40,27 @@ const PaymentDetail: React.FC = () => {
                     className="absolute w-[72px] h-[72px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
                 />
             </div>
+            <div className="mb-8">
+                <p className="text-xl font-semibold">{itemName}</p>
+            </div>
 
             {/* 상세 정보 */}
-            <div className="mt-6">
+            <div>
                 <div className="flex justify-between border-b border-gray-600 py-2">
-                    <span>Date</span>
-                    <span>
+                    <span className="text-[#A3A3A3] text-sm font-normal">Date</span>
+                    <span className="text-base font-semibold">
                         {purchaseDate
                         ? new Date(purchaseDate).toLocaleString()
                         : "--"}
                     </span>
                 </div>
                 <div className="flex justify-between border-b border-gray-600 py-2">
-                    <span>Order ID</span>
-                    <span>{orderId || "--"}</span>
+                    <span className="text-[#A3A3A3] text-sm font-normal">Order ID</span>
+                    <span className="text-base font-semibold">{orderId || "--"}</span>
                 </div>
                 <div className="flex justify-between py-2">
-                    <span>Price</span>
-                    <span>USD ${price ?? "--"}</span>
+                    <span className="text-[#A3A3A3] text-sm font-normal">Price</span>
+                    <span className="text-base font-semibold">USD ${price ?? "--"}</span>
                 </div>
             </div>
         </div>
